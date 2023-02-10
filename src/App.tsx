0c758f1a6c099e9be9a0a63b967f5f5b688f2020
Fabio Filho <fabio.filho@acct.global>
2023-02-10 20:29:52 -0300

    added navbar with bootstrap

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { NavBar } from "./components/Navbar"
+
+import './styles/app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <NavBar />
+  )
+}
+
+export default App
